adf69c7b429f641c58318203d8bcfe7a6d980a21
Dibyendu Dhauri <91137992+Dibyendu-Dhauri@users.noreply.github.com>
2026-07-27 10:42:40 +0530

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.py
@@ -0,0 +1,44 @@
+class DoublyLinkedListNode:
+    def __init__(self,key,val):
+        self.key = key
+        self.val = val
+        self.next = self.prev = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.hashmap = {}
+        self.head = DoublyLinkedListNode(-1,-1)
+        self.tail = DoublyLinkedListNode(-1,-1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def remove_node(self,node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+    
+    def add_tail(self,node):
+        prev_node = self.tail.prev
+        prev_node.next = node
+        node.prev = prev_node
+        node.next = self.tail
+        self.tail.prev = node
+
+    def get(self, key: int) -> int:
+        if key not in self.hashmap:
+            return -1
+        self.remove_node(self.hashmap[key])
+        self.add_tail(self.hashmap[key])
+        return self.hashmap[key].val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.hashmap:
+            self.remove_node(self.hashmap[key])
+        node = DoublyLinkedListNode(key,value)
+        self.hashmap[key] = node
+        if len(self.hashmap) > self.capacity:
+            del self.hashmap[self.head.next.key]
+            self.remove_node(self.head.next)
+            
+        self.add_tail(node)
